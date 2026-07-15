8fbfa22959bdee1725c845b9e37d872fdf733135
ybxjnxfjvc-byte <ybxjnxfjvc@gmail.com>
2026-07-15 22:06:55 +0200

    Delete bot directory

--- bot/dispatcher.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Dispatcher
-from bot.handlers import start, menu, lessons
-
-def setup_dispatcher() -> Dispatcher:
-    dp = Dispatcher()
-    dp.include_router(start.router)
-    dp.include_router(menu.router)
-    dp.include_router(lessons.router)
-    return dp
--- bot/states/learning.py
+++
@@ -1,6 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class LearningStates(StatesGroup):
-    selecting_lesson = State()    # выбор урока
-    answering_question = State()  # ответ на вопрос
-    waiting_for_feedback = State()# обратная связь
